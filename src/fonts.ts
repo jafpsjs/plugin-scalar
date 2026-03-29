5d92d8eadea4b60b9bc2cfeb712144a1e5676ebc
Joshua Avalon <git@avalon.sh>
2026-03-29 17:33:53 +0800

    fix: Update fonts

--- src/fonts.ts
+++ src/fonts.ts
@@ -4,19 +4,25 @@ import { Type } from "typebox";
 import type { FastifyInstance } from "fastify";
 
 export function addFontsRoutes(app: FastifyInstance, prefix: string): void {
-  const fonts = ["montserrat", "roboto"];
-  const sizes = [300, 400, 700];
+  const fonts = ["montserrat", "source-code-pro"];
+  // const files = [
+  //   "cyrillic-ext-wght-normal",
+  //   "cyrillic-wght-normal",
+  //   "greek-ext-wght-normal",
+  //   "greek-wght-normal",
+  //   "vietnamese-wght-normal",
+  //   "latin-ext-wght-normal",
+  //   "latin-wght-normal"
+  // ];
   for (const font of fonts) {
-    const fontRoot = resolve(dirname(fileURLToPath(import.meta.resolve(`@fontsource/${font}`))));
-    for (const size of sizes) {
-      app.get(
-        `${prefix}/fonts/${font}/${size}.css`,
-        { config: { openapi: { hide: true } } },
-        async (_req, res) => {
-          await res.sendFile(`${size}.css`, fontRoot, { immutable: true });
-        }
-      );
-    }
+    const fontRoot = resolve(dirname(fileURLToPath(import.meta.resolve(`@fontsource-variable/${font}`))));
+    app.get(
+      `${prefix}/fonts/${font}/wght.css`,
+      { config: { openapi: { hide: true } } },
+      async (_req, res) => {
+        await res.sendFile("wght.css", fontRoot, { immutable: true });
+      }
+    );
     app.get(
       `${prefix}/fonts/${font}/files/:name.woff2`,
       {
@@ -26,17 +32,6 @@ export function addFontsRoutes(app: FastifyInstance, prefix: string): void {
       async (req, res) => {
         const name = req.params.name;
         await res.sendFile(`files/${name}.woff2`, fontRoot, { immutable: true });
-      }
-    );
-    app.get(
-      `${prefix}/fonts/${font}/files/:name.woff`,
-      {
-        config: { openapi: { hide: true } },
-        schema: { params: Type.Object({ name: Type.String() }) }
-      },
-      async (req, res) => {
-        const name = req.params.name;
-        await res.sendFile(`files/${name}.woff`, fontRoot, { immutable: true });
       }
     );
   }
